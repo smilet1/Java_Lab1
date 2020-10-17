4544914f23479eaf27db2a22a2e6b90d833ad2d8
smilet1 <maxsol2000@gmail.com>
2020-10-17 15:46:04 +0300

    Update

--- lab1/src/main/java/ListLab.java
+++ lab1/src/main/java/ListLab.java
@@ -7,54 +7,54 @@
 
 /**
  *
- * @author maxso
+ * @author Максим Усков
  */
-public class ListLab {
-    public class Node{
-        private int element;
+public class ListLab<T> {
+    private class Node<T>{                           //Элемент списка содержащий значение и указатель на следующий элемент списка
+        private T element;
         private Node next;
-        Node(int e){
+        Node(T e){
         element=e;
         next=null;
         }
     }
-     private Node head; //указатель на первый элемент
-     private Node tail; //указатель на последний элемент
-     ListLab(int e){
+     private Node head;                              //указатель на первый элемент
+     private Node tail;                              //указатель на последний элемент
+     ListLab(T e){
          head=new Node(e);
          tail=head;
      }
-     public void add(int e){ //Дабовление элемента в список
+     public void add(T e){                           //Дабовление элемента в список
         Node tmp=new Node(e);
         tail.next=tmp;
         tail=tail.next;
      }
-     public void printAll(){ // Вывод всех элементов списка
+     public void printAll(){                        // Вывод всех элементов списка
          Node tmp=head;
          while(tmp!=null){
              System.out.print(tmp.element + " ");
              tmp=tmp.next;
          }
      }
-     public int get(int k){                 //Получение определенного элемента списка (отсчет с 1)
+     public<T> T get(int k){                      //Получение определенного элемента списка (отсчет с 1)
          Node tmp=head;
          for(int i=1;i<k;i++){
              tmp=tmp.next;
-             if(tmp==null){                 // Проверка на количество элементов
+             if(tmp==null){                       // Проверка на количество элементов
                  System.out.print("Такого элемента не существует");
-                 return 0;
+                 return null;
              }
          }
-         return tmp.element;
+         return(T) tmp.element;
      }
       public void delete(int k){                 //Удаление определенного элемента списка (отсчет с 1)
         
-         if(k==1){
+         if(k==1){                               //Удаления первого элемента(т.к. для последующий элементов алгоритм отличается)    
              head=head.next;
              return;
          }
-         Node tmp=head;
-         for(int i=1;i<k-1;i++){
+         Node tmp=head;                          
+         for(int i=2;i<k-1;i++){                 //Удаление указанного элемента
              tmp=tmp.next;
              if(tmp==null){             
                  System.out.print("Такого элемента не существует");
@@ -63,7 +63,7 @@ public class ListLab {
          }
          tmp.next=tmp.next.next;
      }
-      public int amountElem(){
+      public int amountElem(){                   //Определяет количество элементов в списке
           Node tmp=head;
           int k=0;
           while(tmp!=null){
--- lab1/src/main/java/main.java
+++ lab1/src/main/java/main.java
@@ -6,16 +6,16 @@
 
 /**
  *
- * @author maxso
+ * @author Максим Усков
  */
 public class main {
     public static void main(String[] args) {
         ListLab N= new ListLab(10);
         N.add(5);
-        N.add(7);
+        N.add("e");
         N.printAll();
         N.delete(2);
-        System.out.print("\n"+N.get(1));
+        System.out.print("\n"+N.get(2));
         System.out.print("\n"+N.amountElem());
         
     }
